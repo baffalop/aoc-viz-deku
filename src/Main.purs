4b83ff03c7ad902b667a521df0aad8daa16cd5bb
Nikita Gaidakov <ngaidakov@podfather.com>
2023-03-12 21:12:16 +0000

    More DOM structure, state

--- src/Main.purs
+++ src/Main.purs
@@ -1,23 +1,48 @@
-module Main where
+module Main
+  ( main
+  )
+  where
 
 import Prelude
 
+import Data.Int (floor)
+import Data.Tuple.Nested ((/\))
+import Deku.Attribute ((!:=), (<:=>))
 import Deku.Attributes (klass_)
-import Deku.Do as Deku
+import Deku.Control (text)
+import Deku.Core (fixed)
 import Deku.DOM as D
-import Deku.Control (text_)
-import Deku.Core (fixed, Nut)
+import Deku.Do as Deku
+import Deku.Hooks (useState)
+import Deku.Listeners (slider_)
 import Deku.Toplevel (runInBody)
 import Effect (Effect)
 import QualifiedDo.Alt as Alt
 
 main :: Effect Unit
-main = runInBody top
+main = runInBody Deku.do
+  setN /\ n <- useState 10
 
-top :: Nut
-top = fixed
-  [ D.div
-      Alt.do
-        klass_ "m-8 p-4 bg-slate-700 text-slate-100 max-w-max"
-    [text_ "Hellooo"]
-  ]
+  fixed
+    [ D.div
+        Alt.do
+          klass_ $ containerKlass <> " space-x-4 max-w-max"
+        [ D.input
+            Alt.do
+              slider_ $ setN <<< floor
+              klass_ "cursor-pointer"
+              D.Value <:=> show <$> n
+              D.Step !:= "1"
+              D.Min !:= "1"
+              D.Max !:= "20"
+          []
+        , D.span_ [text $ show <$> n]
+        ]
+    , D.div
+        Alt.do
+          klass_ $ containerKlass <> " flex-1"
+        []
+    ]
+
+containerKlass :: String
+containerKlass = "p-4 bg-slate-700"
